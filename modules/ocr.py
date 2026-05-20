f8deca85027ccf913818f954d6eb6e1a09b7cbd5
Gaëlle MERIALDO <37674992+gmerialdo@users.noreply.github.com>
2026-05-20 16:49:05 +0200

    Update ocr.py

--- modules/ocr.py
+++ modules/ocr.py
@@ -1,3 +1,16 @@
 import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+)
+
 def extract_text(img):
-    return pytesseract.image_to_string(img)
+
+    try:
+        return pytesseract.image_to_string(
+            img,
+            lang="eng"
+        )
+
+    except Exception as e:
+        return f"OCR error: {str(e)}"
